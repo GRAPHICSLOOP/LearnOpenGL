8db739c9da05139df8a6d367dfd1f9f3d09465cd
GRAPHICSLOOP <Jacsonwy@163.com>
2021-12-25 21:56:51 +0800

    test

--- Engine/Source/ShaderManager/ShaderManager.h
+++ Engine/Source/ShaderManager/ShaderManager.h
@@ -20,7 +20,7 @@ public:
 	// 激活shader
 	void use();
 
-	// uniform工具函数
+	// uniform工具函数1
 	void setBool(const std::string& name, bool value) const;
 	void setInt(const std::string& name, int value) const;
 	void setFloat(const std::string& name, float value) const;
