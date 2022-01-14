3a90bd1510cac95c2437695fa093cb6ee1e0236e
GRAPHICSLOOP <Jacsonwy@163.com>
2022-01-14 17:54:05 +0800

    灰度后期效果

--- Engine/Shader/Framebuffer/FragmentShader.glsl
+++ Engine/Shader/Framebuffer/FragmentShader.glsl
@@ -7,6 +7,6 @@ uniform sampler2D diffuse;
 void main()
 {
 	vec4 textureColor = texture(diffuse,textureCoord);
-
-	FragColor = textureColor;
+	float average = 0.2126 * textureColor.r + 0.7152 * textureColor.g + 0.0722 * textureColor.b;
+	FragColor = vec4(vec3(average),1.0f);
 }
