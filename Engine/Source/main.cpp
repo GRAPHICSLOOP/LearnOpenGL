c48d7318c9cdf5a1c05bfdd2c63cfe1647c8e7d8
GRAPHICSLOOP <Jacsonwy@163.com>
2022-01-17 23:17:52 +0800

    shadowmap-搭建场景

--- Engine/Source/main.cpp
+++ Engine/Source/main.cpp
@@ -44,6 +44,10 @@ unsigned int loadTextureFromFile(const char* texturePath, GLenum clampType);
 // 生成Cube
 unsigned int createCube();
 
+// 生成panel
+unsigned int createPlane();
+
+
 int main()
 {
 	GLFWwindow* window = initWindow((int)screenWidth, (int)screenHeight);
@@ -52,51 +56,21 @@ int main()
 
 	// 加载模型
 	// ------------------------------------------------------------------
-	Model planet("./Model/planet/planet.obj");
-	Model rock("./Model/rock/rock.obj");
+	unsigned int cubeVAO = createCube();
+	unsigned int planeVAO = createPlane();
+
 
 	// 加载材质
 	// ------------------------------------------------------------------
 	ShaderManager shaderInstance("./Engine/Shader/Instancing/VertexModelShader.glsl", "./Engine/Shader/Instancing/FragmentModelShader.glsl");
-	ShaderManager shader("./Engine/Shader/Model/VertexShader.glsl", "./Engine/Shader/Model/FragmentSimpleShader.glsl");
+	ShaderManager shader("./Engine/Shader/ShadowMap/VertexShader.glsl", "./Engine/Shader/ShadowMap/FragmentSimpleShader.glsl");
 
-	// 设置陨石位置
-	const unsigned int amount = 100000;
-	glm::mat4* modelMatrices;
-	modelMatrices = new glm::mat4[amount];
-	srand((unsigned int)glfwGetTime()); // 初始化随机种子    
-	float radius = 20.0;
-	float offset = 10.5f;
-	for (unsigned int i = 0; i < amount; i++)
-	{
-		glm::mat4 model = glm::mat4(1.f);
-		glm::mat4 ins = glm::mat4(1.f);
-
-		// 1. 缩放：在 0.05 和 0.25f 之间缩放
-		float scale = (float)((rand() % 10) / 1000.0f + 0.001);
-		model = glm::scale(ins, glm::vec3(scale));
-
-		// 2. 旋转：绕着一个（半）随机选择的旋转轴向量进行随机的旋转
-		float rotAngle = (float)(rand() % 360);
-		model = glm::rotate(ins, rotAngle, glm::vec3(0.4f, 0.6f, 0.8f)) * model;
-
-		// 3. 位移：分布在半径为 'radius' 的圆形上，偏移的范围是 [-offset, offset]
-		float angle = (float)i / (float)amount * 360.0f;
-		float displacement = (rand() % (int)(2 * offset * 100)) / 100.0f - offset;
-		float x = sin(angle) * radius + displacement;
-		displacement = (rand() % (int)(2 * offset * 100)) / 100.0f - offset;
-		float y = displacement * 0.4f; // 让行星带的高度比x和z的宽度要小
-		displacement = (rand() % (int)(2 * offset * 100)) / 100.0f - offset;
-		float z = cos(angle) * radius + displacement;
-		model = glm::translate(ins, glm::vec3(x, y, z)) * model;
-
-		// 4. 添加到矩阵的数组中
-		modelMatrices[i] = model;
-	}
-	rock.setInstance(modelMatrices, amount);
+	// 加载贴图
+	unsigned int textureCube = loadTextureFromFile("./resources/textures/toy_box_diffuse.png", GL_REPEAT);
+	unsigned int texturePlane = loadTextureFromFile("./resources/textures/wood.png", GL_REPEAT);
 
 	glEnable(GL_DEPTH_TEST);
-	glEnable(GL_MULTISAMPLE);
+	//glEnable(GL_CULL_FACE);
 	while (!glfwWindowShouldClose(window))
 	{
 		// 每帧开始时计算时间
@@ -112,22 +86,26 @@ int main()
 		// ------------------------------------------------------------------
 		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
 		shader.use();
-		setModelTransform(shader, glm::vec3(0.f), glm::vec3(1.f), 0.f);
-		planet.Draw(&shader);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, textureCube);
+		shader.setInt("material.diffuse0", 0);
 
-		shaderInstance.use();
-		// 绘制陨石
-		glm::mat4 viewMatrix = glm::mat4(1.0f);
-		viewMatrix = cameraManager.getLookAtMatrix();
+		glBindVertexArray(cubeVAO);
+		setModelTransform(shader, glm::vec3(0.0f, 1.5f, 0.0f), glm::vec3(0.6f), 36.f);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		setModelTransform(shader, glm::vec3(2.0f, 0.0f, 1.0f), glm::vec3(0.6f), 0.f);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		setModelTransform(shader, glm::vec3(-1.0f, 0.0f, 2.0f), glm::vec3(0.6f), 0.f);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
 
-		glm::mat4 projectionMatrix = glm::mat4(1.0f);
-		projectionMatrix = glm::perspective(glm::radians(45.f), screenWidth / screenHeight, 0.1f, 100.f);
+		glBindTexture(GL_TEXTURE_2D, texturePlane);
+		shader.setInt("material.diffuse0", 0);
+		glBindVertexArray(planeVAO);
+		setModelTransform(shader, glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.f), 0.f);
+		glDrawArrays(GL_TRIANGLES, 0, 6);
 
-
-		shaderInstance.setMatrix("viewMatrix", viewMatrix);
-		shaderInstance.setMatrix("projectionMatrix", projectionMatrix);
-		rock.DrawInstance(&shaderInstance, amount);
 
 		// swapbuffer
 		glfwSwapBuffers(window);
@@ -369,4 +347,35 @@ unsigned int createCube()
 	glBindVertexArray(0);
 
 	return cubeVAO;
+}
+
+unsigned int createPlane()
+{
+	unsigned int planeVAO, planeVBO;
+
+	float planeVertices[] = {
+		// positions            // normals         // texcoords
+		 25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,  25.0f,  0.0f,
+		-25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,   0.0f,  0.0f,
+		-25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
+
+		 25.0f, -0.5f,  25.0f,  0.0f, 1.0f, 0.0f,  25.0f,  0.0f,
+		-25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,   0.0f, 25.0f,
+		 25.0f, -0.5f, -25.0f,  0.0f, 1.0f, 0.0f,  25.0f, 25.0f
+	};
+	// plane VAO
+	glGenVertexArrays(1, &planeVAO);
+	glGenBuffers(1, &planeVBO);
+	glBindVertexArray(planeVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, planeVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(planeVertices), planeVertices, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(2);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glBindVertexArray(0);
+
+	return planeVAO;
 }
